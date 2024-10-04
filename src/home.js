e6b388b800df5470ae70396290987fec257a41c2
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-10-04 16:38:00 +0200

    Create Homepage in js

--- src/home.js
+++ src/home.js
@@ -4,5 +4,6 @@ export {createHome}
 function createHome() {
     const container = document.querySelector("#container");
 
-    const h2 = createElement("h2", "", "", "Hopefully this worked!", container)
+    const title = createElement("h2", "", "", "Home", container)
+    const homeText = createElement("p", "", "", "Welcome to Odin's Steakhouse, where flavor meets perfection! Our steaks are hand-selected from the finest cuts of beef, aged to perfection, and expertly grilled to bring out rich, smoky flavors that melt in your mouth. Whether you're craving a juicy ribeye, a tender filet mignon, or our signature T-bone steak, we promise an unforgettable dining experience. Complement your meal with our carefully curated selection of fine wines and decadent sides. At Odin Steakhouse, we believe in serving only the best, so come indulge in a steakhouse experience like no other.", container)
 }
